a13a0ba2a8ae3c2ad018a78b52cd0435ff97e4a6
jannik-boeppli <69905162+jannik-boeppli@users.noreply.github.com>
2022-11-03 10:31:57 +0100

    Added blob controller and replaced application.properties with application.yml and added a page as return when accessing the page

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,17 +5,40 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServlet;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication extends HttpServlet {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
-	String sayHello() {
-		return "<p>This is a test</p>!";
+	String getPage() {
+		return PAGE_HEADER + FILE_UPLOAD + PAGE_FOOTER;
 	}
+
+	private String PAGE_HEADER = '
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+    		<meta charset="UTF-8">
+    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    		<title>File Upload</title>
+		</head>
+		<body>'; 
+
+	private String FILE_UPLOAD = '
+		<h1>This is a HTML header</h1>
+    	<h3>File upload:</h3>
+    	<div>Select a file to upload:</div>
+    	<form action="UploadServlet" method="post" enctype="multipart/form-data">
+        	<input type="file" name="file" size="50"/>
+        	<input type="submit" value="Upload File" />
+    	</form>';
+
+	private String PAGE_FOOTER = '</body></html>';
 }
