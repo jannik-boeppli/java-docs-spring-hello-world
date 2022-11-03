58a3ffa3d0ebbc91e8563ac6c4f723794546ab6f
jannik-boeppli <69905162+jannik-boeppli@users.noreply.github.com>
2022-11-03 13:50:28 +0100

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,27 +10,7 @@ import javax.servlet.http.HttpServlet;
 @SpringBootApplication
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
-	private String PAGE_HEADER = '
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-    		<meta charset="UTF-8">
-    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    		<title>File Upload</title>
-		</head>
-		<body>'; 
 
-	private String FILE_UPLOAD = '
-		<h1>This is a HTML header</h1>
-    	<h3>File upload:</h3>
-    	<div>Select a file to upload:</div>
-    	<form action="UploadServlet" method="post" enctype="multipart/form-data">
-        	<input type="file" name="file" size="50"/>
-        	<input type="submit" value="Upload File" />
-    	</form>';
-
-	private String PAGE_FOOTER = '</body></html>';
 
 
 	public static void main(String[] args) {
